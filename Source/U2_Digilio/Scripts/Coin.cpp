dbed862cd3b0388286bedae5e48c563a3217b9e0
Juan Digilio <juanignaciodigilio@gmail.com>
2025-10-03 22:37:12 -0300

    Added Coin Class

---
+++ Source/U2_Digilio/Scripts/Coin.cpp
@@ -0,0 +1,76 @@
+
+#include "Coin.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/PointLightComponent.h"
+#include "GameFramework/Character.h"
+
+ACoin::ACoin()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	CollisionBox->SetupAttachment(RootComponent);
+	CollisionBox->SetBoxExtent(FVector(50.f));
+	CollisionBox->SetCollisionProfileName("Trigger");
+	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ACoin::OnOverlapBegin);
+
+	AuraMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("AuraMesh"));
+	AuraMesh->SetupAttachment(RootComponent);
+	AuraMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	WasCollected = false;
+}
+
+void ACoin::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AuraMesh)
+	{
+		UMaterialInstanceDynamic* DynMat = AuraMesh->CreateAndSetMaterialInstanceDynamic(0);
+
+		if (DynMat)
+		{
+			DynMat->SetScalarParameterValue("GlowIntensity", 3.f);
+		}
+
+		AuraMaterial = AuraMesh->CreateAndSetMaterialInstanceDynamic(0);
+		if (AuraMaterial)
+		{
+			AuraMaterial->SetScalarParameterValue("GlowIntensity", 3.f);
+		}
+	}
+}
+
+void ACoin::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!WasCollected)
+	{
+		AddActorLocalRotation(FRotator(0.f, 0.f, RotationSpeed * DeltaTime));
+	}
+}
+
+void ACoin::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (WasCollected) return;
+
+	if (OtherActor && OtherActor != this && OtherActor->IsA(ACharacter::StaticClass()))
+	{
+		WasCollected = true;
+
+		AuraMesh->SetVisibility(false);
+		Mesh->SetVisibility(false);
+		Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		CollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+		UE_LOG(LogTemp, Warning, TEXT("Moneda colectada!"));
+	}
+}
---
+++ Source/U2_Digilio/Scripts/Coin.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Coin.generated.h"
+
+UCLASS()
+class U2_DIGILIO_API ACoin : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Coin")
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Coin")
+	class UBoxComponent* CollisionBox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Coin")
+	float RotationSpeed = 180.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Coin")
+	bool WasCollected = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* AuraMesh;
+
+	UPROPERTY()
+	UMaterialInstanceDynamic* AuraMaterial;
+
+	ACoin();
+
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+		bool bFromSweep, const FHitResult& SweepResult);
+
+protected:
+
+	virtual void BeginPlay() override;
+};
